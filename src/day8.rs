556079e8369fd745163dd3116acc597945e596bf
DevSlashRichie <me@slashrichie.dev>
2025-12-09 01:03:03 -0600

    finish day 8 p1

--- src/day8.rs
+++ src/day8.rs
@@ -1,11 +1,15 @@
+use std::collections::{HashMap, HashSet};
+
 use crate::utils::read_file_contents;
 use itertools::Itertools;
 use min_heap::MinHeap;
 use rustc_hash::FxHashMap;
 
-type Coord = (i64, i64, i64);
+pub fn sig(a: (i64, i64, i64)) -> i64 {
+    a.0.pow(2) + a.1.pow(2) + a.2.pow(3)
+}
 
-pub fn euclidean_distance(a: Coord, b: Coord) -> usize {
+pub fn euclidean_distance(a: (i64, i64, i64), b: (i64, i64, i64)) -> usize {
     let n = (b.0 - a.0).pow(2) + (b.1 - a.1).pow(2) + (b.2 - a.2).pow(2);
 
     (n as f64).sqrt() as usize
@@ -15,8 +19,8 @@ pub fn run(second: bool) {
     let contents = read_file_contents("day8.txt");
     let numbers = contents
         .lines()
-        .map(|coord| {
-            let mut numbers = coord
+        .map(|f| {
+            let mut numbers = f
                 .split(',')
                 .map(|num| i64::from_str_radix(num, 10).unwrap());
 
@@ -28,76 +32,61 @@ pub fn run(second: bool) {
         })
         .collect::<Vec<_>>();
 
+    let mut links = FxHashMap::<(i64, i64, i64), i64>::default();
+    let mut circuits = FxHashMap::<i64, Vec<(i64, i64, i64)>>::default();
+
     let mut heap = numbers
         .iter()
         .flat_map(|coord| {
             let distances = numbers
                 .iter()
-                .filter(|other| other != &coord && coord < other)
-                .map(|other| (euclidean_distance(*coord, *other), coord, other))
+                .filter(|other| *other != coord && coord < *other)
+                .map(|other| (euclidean_distance(*coord, *other), *coord, *other))
                 .collect::<Vec<_>>();
 
             distances
         })
         .collect::<MinHeap<_>>();
 
-    let mut parents = FxHashMap::default();
-    let mut size = FxHashMap::default();
+    println!("{}", heap.len());
 
-    for num in &numbers {
-        size.insert(*num, 1);
-        parents.insert(*num, *num);
-    }
-
-    fn find(coord: Coord, parents: &mut FxHashMap<Coord, Coord>) -> Coord {
-        if let Some(found) = parents.get(&coord) {
-            if &coord == found {
-                return coord;
-            }
-
-            let f = find(*found, parents);
-            parents.insert(coord, f);
-
-            return f;
-        }
-
-        (0, 0, 0)
-    }
-
-    for _ in 0..10 {
+    for _ in 0..1000 {
         if let Some((_, a, b)) = heap.pop() {
-            let root_a = find(*a, &mut parents);
-            let root_b = find(*b, &mut parents);
+            let a_circuit = *links.get(&a).unwrap_or(&sig(a));
+            let b_circuit = *links.get(&b).unwrap_or(&sig(b));
 
-            if root_a == root_b {
+            if a_circuit == b_circuit {
                 continue;
             }
 
-            let (p1, p2) = match size.get(&root_a).zip(size.get(&root_b)) {
-                Some((a, b)) => {
-                    if a < b {
-                        (root_a, root_b)
-                    } else {
-                        (root_b, root_a)
-                    }
-                }
-                None => continue,
+            let a_childs = circuits.get(&a_circuit).cloned().unwrap_or(vec![a]);
+            let b_childs = circuits.get(&b_circuit).cloned().unwrap_or(vec![b]);
+
+            let choosen_id = if a_childs.len() < b_childs.len() {
+                b_circuit
+            } else {
+                a_circuit
             };
 
-            if let Some(p1_size) = size.get(&p1).copied() {
-                size.entry(p2).and_modify(|value| *value += p1_size);
-                parents.insert(p1, p2);
+            let all_childs = a_childs
+                .iter()
+                .chain(b_childs.iter())
+                .collect::<HashSet<_>>();
+
+            for coord in &all_childs {
+                links.insert(**coord, choosen_id);
             }
+
+            circuits.insert(choosen_id, all_childs.into_iter().copied().collect());
         }
     }
 
-    let ans = parents
-        .iter()
-        .filter(|(a, b)| a == b)
-        .map(|(entry, _)| size.get(entry).copied().unwrap())
+    let ans = circuits
+        .into_values()
+        .map(|f| f.len())
         .sorted_unstable_by(|a, b| b.cmp(a))
         .take(3)
-        .product::<i64>();
+        .product::<usize>();
 
-    println!("ANS: {:#?}", ans);
+    println!("ANS: {}", ans);
 }
